222c5634629eed0989821ef49bcee6c501b5c6c5
triven312 <triven312@gmail.com>
2016-10-04 18:09:55 +0530

    Modifications to submit api
    Modifications to submit api

--- server.js
+++ server.js
@@ -32,9 +32,46 @@ app.get('/getTableData', function(req, res){
 });
 
 app.post('/submit', function(req, res){
-  console.log(req.body);
+  queryA = "SELECT * FROM tournament_standings where name='"+ req.body.teamA+"'";
+  queryB = "SELECT * FROM tournament_standings where name='"+ req.body.teamB+"'";
+  pool.getConnection(function(err, connection) {
+    console.log(queryA);
+    connection.query(queryA, function(err, rows, fields) {
+      if (!err) {
+        console.log("Entered here 1");
+        if(rows.length == 0){
+          console.log("Entered here 2");
+          insertTeamA="INSERT into sys.tournament_standings (name, points, matches, won, lost, draw) values ('"+req.body.teamA+"',0,0,0,0,0)"
+          connection.query( insertTeamA, function(err, rows, fields) {
+            if (!err) {
+              connection.release();
+              console.log("Entered here 3");
+            }
+            else{
+              console.log('Error while performing Query. 1');
+            }
+          });
+        }
+      }
+      else{
+        console.log('Error while performing Query. 2');
+      }
+    });
+  });
+  if(req.body.teamAScore > req.body.teamBScore){
+    console.log("Team A is the winner");
+  }
+  else{
+    if(req.body.teamBScore > req.body.teamAScore){
+      console.log("Team B is the winner");
+    }
+    else {
+      console.log("Match is drawn");
+    }
+  }
+  var output = "";
   output=output+("<br><p>Thankyou for the submission</p><br>");
-  res.send(200, output);
+  res.status(200).send(output);
 });
 
 require('http').createServer(app).listen(3000, function(){
